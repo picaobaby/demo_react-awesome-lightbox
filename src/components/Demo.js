9ce3c61563bd3fd73e73846b32219981089ddb5b
Picao Baby <lioncubs11@gmail.com>
2020-09-30 22:03:26 -0400

    try again

---
+++ src/components/Demo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Lightbox from 'react-awesome-lightbox';
+import "react-awesome-lightbox/build/style.css";
+
+const Demo = () => {
+    return (
+        <Lightbox 
+            image="https://i.dailymail.co.uk/1s/2020/10/01/00/33834856-0-image-a-32_1601508565199.jpg" 
+            title="title one here" 
+        />
+    ) 
+}
+
+export default Demo
